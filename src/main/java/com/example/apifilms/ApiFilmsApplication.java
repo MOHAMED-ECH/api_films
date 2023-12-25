3174ccc38c78f49f8ea352beae2d6795ebf05894
MOHAMED-ECH <echarfaouy.mohamed@student.emi.ac.ma>
2023-12-25 01:51:43 +0100

    AA

---
+++ src/main/java/com/example/apifilms/ApiFilmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.apifilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFilmsApplication.class, args);
+	}
+
+}
